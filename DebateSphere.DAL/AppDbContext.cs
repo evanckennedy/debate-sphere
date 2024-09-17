f89f4a7df68978898cfee1784bfe72721cde9706
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-17 14:55:18 -0500

    create db context

---
+++ DebateSphere.DAL/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using DebateSphere.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebateSphere.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        // contstructor
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // properties
+        public DbSet<User> Users { get; set; }
+        public DbSet<Debate> Debates { get; set; }
+        public DbSet<Argument> Arguments { get; set; }
+        public DbSet<Vote> Votes { get; set; }
+
+        // OnModelCreating method
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // configuring the database schema and relationships
+        }
+    }
+}
