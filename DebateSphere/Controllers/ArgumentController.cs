c2ed5299a0b093f3d08513b96efeb97e27a34d49
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-18 11:08:31 -0500

    create controller files

---
+++ DebateSphere/Controllers/ArgumentController.cs
@@ -0,0 +1,13 @@
+﻿using DebateSphere.BLL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DebateSphere.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArgumentController : ControllerBase
+    {
+        
+    }
+}
---
+++ DebateSphere/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using DebateSphere.BLL;
+using DebateSphere.Models;
+using DebateSphere.Models.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DebateSphere.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+    }
+}
