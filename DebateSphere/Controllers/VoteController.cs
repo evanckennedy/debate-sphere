c8bd56adc5cd1e090b3f849f64999b9f3d2339f1
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-19 18:34:50 -0500

    configure vote controller

--- DebateSphere/Controllers/VoteController.cs
+++ DebateSphere/Controllers/VoteController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DebateSphere.BLL.Interfaces;
+using DebateSphere.Models.DTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DebateSphere.Controllers
@@ -7,5 +9,26 @@ namespace DebateSphere.Controllers
     [ApiController]
     public class VoteController : ControllerBase
     {
+        private readonly IVoteService _voteService;
+
+        public VoteController(IVoteService voteService)
+        {
+            _voteService = voteService;
+        }
+
+        [HttpPost("/debates/{debateId}/vote")]
+        public async Task<IActionResult> CreateVote(int debateId, VoteCreateDTO voteCreateDTO)
+        {
+            voteCreateDTO.DebateID = debateId;
+            var vote = await _voteService.CreateVoteAsync(voteCreateDTO);
+            return CreatedAtAction(nameof(GetVotesByDebateId), new { debateId = vote.DebateID }, vote);
+        }
+
+        [HttpGet("/debates/{debateId}/votes")]
+        public async Task<IActionResult> GetVotesByDebateId(int debateId)
+        {
+            var votes = await _voteService.GetVotesByDebateIdAsync(debateId);
+            return Ok(votes);
+        }
     }
 }
