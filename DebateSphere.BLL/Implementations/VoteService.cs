20e34c9d36838b3b0493df052a14fb27eb1b3f2d
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-19 18:27:03 -0500

    configure VoteService

--- DebateSphere.BLL/Implementations/VoteService.cs
+++ DebateSphere.BLL/Implementations/VoteService.cs
@@ -1,4 +1,8 @@
-﻿using DebateSphere.BLL.Interfaces;
+﻿using AutoMapper;
+using DebateSphere.BLL.Interfaces;
+using DebateSphere.DAL.Interfaces;
+using DebateSphere.Models;
+using DebateSphere.Models.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +13,27 @@ namespace DebateSphere.BLL.Implementations
 {
     public class VoteService : IVoteService
     {
+        private readonly IVoteDAL _voteDAL;
+        private readonly IMapper _mapper;
+
+        public VoteService(IVoteDAL voteDAL, IMapper mapper)
+        {
+            _voteDAL = voteDAL;
+            _mapper = mapper;
+        }
+
+        public async Task<VoteListDTO> CreateVoteAsync(VoteCreateDTO voteCreateDTO)
+        {
+            var vote = _mapper.Map<Vote>(voteCreateDTO);
+            vote.CreatedAt = DateTime.UtcNow;
+            var createdVote = await _voteDAL.CreateVoteAsync(vote);
+            return _mapper.Map<VoteListDTO>(createdVote);
+        }
+
+        public async Task<IEnumerable<VoteListDTO>> GetVotesByDebateIdAsync(int debateId)
+        {
+            var votes = await _voteDAL.GetVotesByDebateIdAsync(debateId);
+            return _mapper.Map<IEnumerable<VoteListDTO>>(votes);
+        }
     }
 }
