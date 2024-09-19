9f8925476a66248a22f630c7faab050146d45372
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-19 18:08:39 -0500

    configure vote DAL

--- DebateSphere.DAL/Implementations/VoteDAL.cs
+++ DebateSphere.DAL/Implementations/VoteDAL.cs
@@ -1,4 +1,6 @@
 ﻿using DebateSphere.DAL.Interfaces;
+using DebateSphere.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,25 @@ namespace DebateSphere.DAL.Implementations
 {
     public class VoteDAL: IVoteDAL
     {
+        private readonly AppDbContext _context;
+
+        public VoteDAL (AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Vote> CreateVoteAsync(Vote vote)
+        {
+            _context.Votes.Add(vote);
+            await _context.SaveChangesAsync();
+            return vote;
+        }
+
+        public async Task<IEnumerable<Vote>> GetVotesByDebateIdAsync(int debateId)
+        {
+            return await _context.Votes
+                .Where(v => v.DebateID == debateId)
+                .ToListAsync();
+        }
     }
 }
