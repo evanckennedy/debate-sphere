fda11755747ab43711452dec6c48cce00c767804
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-19 15:25:23 -0500

    configure argument service

--- DebateSphere.BLL/Implementations/ArgumentService.cs
+++ DebateSphere.BLL/Implementations/ArgumentService.cs
@@ -1,4 +1,8 @@
-﻿using DebateSphere.BLL.Interfaces;
+﻿using AutoMapper;
+using DebateSphere.BLL.Interfaces;
+using DebateSphere.DAL.Interfaces;
+using DebateSphere.Models;
+using DebateSphere.Models.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +13,46 @@ namespace DebateSphere.BLL.Implementations
 {
     public class ArgumentService : IArgumentService
     {
+        private readonly IArgumentDAL _argumentDAL;
+        private readonly IMapper _mapper;
+
+        public ArgumentService(IArgumentDAL argumentDAL, IMapper mapper)
+        {
+            _argumentDAL = argumentDAL;
+            _mapper = mapper;
+        }
+
+        public async Task<ArgumentReadDTO> CreateArgumentAsync(ArgumentCreateDTO argumentCreateDTO)
+        {
+            var argument = _mapper.Map<Argument>(argumentCreateDTO);
+            argument.CreatedAt = DateTime.UtcNow;
+            var createdArgument = await _argumentDAL.CreateArgumentAsync(argument);
+            return _mapper.Map<ArgumentReadDTO>(argument);
+        }
+
+        public async Task<IEnumerable<ArgumentListDTO>> GetArgumentsByDebateIdAsync(int debateId)
+        {
+            var arguments = await _argumentDAL.GetArgumentsByDebateIdAsync(debateId);
+            return _mapper.Map<IEnumerable<ArgumentListDTO>>(arguments);
+        }
+
+        public async Task<ArgumentReadDTO> GetArgumentByIdAsync(int argumentId)
+        {
+            var argument = await _argumentDAL.GetArgumentByIdAsync(argumentId);
+            return _mapper.Map<ArgumentReadDTO>(argument);
+        }
+
+        public async Task<ArgumentReadDTO> UpdateArgumentAsync(int argumentId, ArgumentUpdateDTO argumentUpdateDTO)
+        {
+            var argument = _mapper.Map<Argument>(argumentUpdateDTO);
+            argument.ArgumentID = argumentId;
+            var updatedArgument = await _argumentDAL.UpdateArgumentAsync(argument);
+            return _mapper.Map<ArgumentReadDTO>(updatedArgument);
+        }
+
+        public async Task<bool> DeleteArgumentAsync(int argumentId)
+        {
+            return await _argumentDAL.DeleteArgumentAsync(argumentId);
+        }
     }
 }
