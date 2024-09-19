7811cb057ca4a598680d7208e663a7d8300c39b5
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-19 18:13:18 -0500

    configure vote service interface

--- DebateSphere.BLL/Interfaces/IVoteService.cs
+++ DebateSphere.BLL/Interfaces/IVoteService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DebateSphere.Models.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace DebateSphere.BLL.Interfaces
 {
     public interface IVoteService
     {
+        Task<VoteListDTO> CreateVoteAsync(VoteCreateDTO voteCreateDTO);
+        Task<IEnumerable<VoteListDTO>> GetVotesByDebateIdAsync(int debateId);
     }
 }
