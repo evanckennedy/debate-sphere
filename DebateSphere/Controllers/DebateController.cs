859f95ea09f52a4d5264adf284ce1193a6e1d957
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-18 22:21:47 -0500

    create debate controller

--- DebateSphere/Controllers/DebateController.cs
+++ DebateSphere/Controllers/DebateController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DebateSphere.BLL.Interfaces;
+using DebateSphere.Models.DTOs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DebateSphere.Controllers
@@ -7,6 +9,58 @@ namespace DebateSphere.Controllers
     [ApiController]
     public class DebateController : ControllerBase
     {
+        private readonly IDebateService _debateService;
 
+        public DebateController (IDebateService debateService)
+        {
+            _debateService = debateService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateDebate(DebateCreateDTO debateCreateDTO)
+        {
+            var debate = await _debateService.CreateDebateAsync(debateCreateDTO);
+            return CreatedAtAction(nameof(GetDebateById), new { debateId = debate.DebateID }, debate);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllDebates()
+        {
+            var debates = await _debateService.GetAllDebatesAsync();
+            return Ok(debates);
+        }
+
+        [HttpGet("{debateId}")]
+        public async Task<IActionResult> GetDebateById(int debateId)
+        {
+            var debate = await _debateService.GetDebateByIdAsync(debateId);
+            if (debate == null)
+            {
+                return NotFound();
+            }
+            return Ok(debate);
+        }
+
+        [HttpPut("{debateId}")]
+        public async Task<IActionResult> UpdateDebate(int debateId, DebateUpdateDTO debateUpdateDTO)
+        {
+            var debate = await _debateService.UpdateDebateAsync(debateId, debateUpdateDTO);
+            if (debate == null)
+            {
+                return NotFound();
+            }
+            return Ok(debate);
+        }
+
+        [HttpDelete("{debateId}")]
+        public async Task<IActionResult> DeleteDebate(int debateId)
+        {
+            var result = await _debateService.DeleteDebateAsync(debateId);
+            if (!result)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
     }
 }
