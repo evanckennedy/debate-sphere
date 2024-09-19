0139b269c80e8d0189f1872c0441b1fbd1372f41
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-19 18:02:58 -0500

    configure voteDAL interface

--- DebateSphere.DAL/Interfaces/IVoteDAL.cs
+++ DebateSphere.DAL/Interfaces/IVoteDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DebateSphere.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,7 @@ namespace DebateSphere.DAL.Interfaces
 {
     public interface IVoteDAL
     {
+        Task<Vote> CreateVoteAsync(Vote vote);
+        Task<IEnumerable<Vote>> GetVotesByDebateIdAsync(int debateId);
     }
 }
