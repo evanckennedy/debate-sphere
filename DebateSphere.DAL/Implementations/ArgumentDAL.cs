a029a3d4229768fa3b7b724a5e1ad9166bbaa1fa
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-19 14:51:36 -0500

    configure argument DAL

--- DebateSphere.DAL/Implementations/ArgumentDAL.cs
+++ DebateSphere.DAL/Implementations/ArgumentDAL.cs
@@ -1,4 +1,6 @@
 ﻿using DebateSphere.DAL.Interfaces;
+using DebateSphere.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,50 @@ namespace DebateSphere.DAL.Implementations
 {
     public class ArgumentDAL : IArgumentDAL
     {
+        private readonly AppDbContext _context;
+
+        public ArgumentDAL(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Argument> CreateArgumentAsync(Argument argument)
+        {
+            _context.Arguments.Add(argument);
+            await _context.SaveChangesAsync();
+            return argument;
+        }
+
+        public async Task<IEnumerable<Argument>> GetArgumentsByDebateIdAsync(int debateId)
+        {
+            return await _context.Arguments
+                .Where(a => a.DebateID == debateId)
+                .ToListAsync();
+        }
+
+        public async Task<Argument> GetArgumentByIdAsync(int argumentId)
+        {
+            return await _context.Arguments
+                .FirstOrDefaultAsync(a => a.ArgumentID == argumentId);
+        }
+
+        public async Task<Argument> UpdateArgumentAsync(Argument argument)
+        {
+            _context.Arguments.Update(argument);
+            await _context.SaveChangesAsync();
+            return argument;
+        }
+
+        public async Task<bool> DeleteArgumentAsync(int argumentId)
+        {
+            var argument = await _context.Arguments.FindAsync(argumentId);
+            if (argument == null)
+            {
+                return false;
+            }
+            _context.Arguments.Remove(argument);
+            await _context.SaveChangesAsync();
+            return true;
+        }
     }
 }
