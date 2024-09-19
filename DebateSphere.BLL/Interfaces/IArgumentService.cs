a4acfdabfe3278df818e7e18ea0ea043bc020f24
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-19 15:01:25 -0500

    configure argument service interface

--- DebateSphere.BLL/Interfaces/IArgumentService.cs
+++ DebateSphere.BLL/Interfaces/IArgumentService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DebateSphere.Models;
+using DebateSphere.Models.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace DebateSphere.BLL.Interfaces
 {
     public interface IArgumentService
     {
+        Task<ArgumentReadDTO> CreateArgumentAsync(ArgumentCreateDTO argumentCreateDTO);
+        Task<IEnumerable<ArgumentListDTO>> GetArgumentsByDebateIdAsync(int debateId);
+        Task<ArgumentReadDTO> GetArgumentByIdAsync(int argumentId);
+        Task<ArgumentReadDTO> UpdateArgumentAsync(int argumentId, ArgumentUpdateDTO argumentUpdateDTO);
+        Task<bool> DeleteArgumentAsync(int argumentId);
     }
 }
