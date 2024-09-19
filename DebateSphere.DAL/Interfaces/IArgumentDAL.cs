ac8f0d196cdcc9b105d165615dca7c4ac757d18e
Evan Kennedy <evanckennedy@protonmail.com>
2024-09-19 14:07:22 -0500

    configure Argument dal interface

--- DebateSphere.DAL/Interfaces/IArgumentDAL.cs
+++ DebateSphere.DAL/Interfaces/IArgumentDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DebateSphere.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace DebateSphere.DAL.Interfaces
 {
     public interface IArgumentDAL
     {
+        Task<Argument> CreateArgumentAsync(Argument argument);
+        Task<IEnumerable<Argument>> GetArgumentsByDebateIdAsync(int debateId);
+        Task<Argument> GetArgumentByIdAsync(int argumentId);
+        Task<Argument> UpdateArgumentAsync(Argument argument);
+        Task<bool> DeleteArgumentAsync(int argumentId);
     }
 }
